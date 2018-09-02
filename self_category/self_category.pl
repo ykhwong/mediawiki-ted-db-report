70829738662b45b0c2dbe457834a2be9fdac4058
ykhwong <ykhwong@daum.net>
2018-09-02 10:09:52 +0900

    [ADD] Self category

---
+++ self_category/self_category.pl
@@ -0,0 +1,60 @@
+use strict;
+use warnings;
+use Time::Piece;
+use POSIX qw(tzset);
+use DBI;
+ 
+my $host='kowiki.analytics.db.svc.eqiad.wmflabs';
+my $dbname='kowiki_p';
+my $default_file='~/replica.my.cnf';
+
+# Strings
+my $timezone_str = '%Y년 %m월 %d일 %H:%M (KST)';
+my $timezone_area = 'Asia/Seoul';
+my $report_template = '
+마지막 갱신: <onlyinclude>%s</onlyinclude>
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! 순번
+! 분류 이름
+! 포함하는 페이지 수
+! 하위 분류 수
+|-
+%s
+|}
+';
+
+my $conn = DBI->connect("DBI:mysql:database=$dbname;host=$host;mysql_read_default_file=$default_file");
+my $cursor = $conn->prepare("
+SELECT
+  page_title,
+  cat_pages,
+  cat_subcats
+FROM page
+JOIN categorylinks
+ON cl_to = page_title
+RIGHT JOIN category
+ON cat_title = page_title
+WHERE page_id = cl_from
+AND page_namespace = 14;
+");
+$cursor->execute();
+
+my $i = 1;
+my @output = ();
+while (my $row = $cursor->fetchrow_hashref()) {
+	my $page_title = sprintf("[[:분류:%s]] || %s || %s", $row->{'page_title'}, $row->{'cat_pages'}, $row->{'cat_subcats'});
+	my $table_row = sprintf("| %d\n| %s\n|-", $i, $page_title);
+	push @output, $table_row;
+	$i++;
+}
+
+$ENV{TZ} = $timezone_area;
+my $current_of = localtime->strftime($timezone_str);;
+my $final_result = sprintf($report_template, $current_of, join("\n", @output));
+
+$cursor->finish();
+$conn->disconnect();
+
+printf("%s", $final_result);
