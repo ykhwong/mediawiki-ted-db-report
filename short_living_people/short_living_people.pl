ce0d21edf8421f8a21fbe526baa138daf7cec11f
ykhwong <ykhwong@daum.net>
2018-09-08 14:34:32 +0900

    [ADD] short_living_people

---
+++ short_living_people/short_living_people.pl
@@ -0,0 +1,63 @@
+use strict;
+use warnings;
+use Time::Piece;
+use POSIX qw(tzset);
+use POSIX qw(locale_h);
+use locale;
+use DBI;
+
+my $host='kowiki.analytics.db.svc.eqiad.wmflabs';
+my $dbname='kowiki_p';
+my $default_file='~/replica.my.cnf';
+
+# Strings
+my $timezone_str = '%Y년 %-m월 %-d일 (%a) %H:%M (KST)';
+my $timezone_area = 'Asia/Seoul';
+my $report_template = '
+마지막 갱신: <onlyinclude>%s</onlyinclude>
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! 순번
+! 문서 이름
+! 길이
+|-
+%s
+|}
+';
+
+my $conn = DBI->connect("DBI:mysql:database=$dbname;host=$host;mysql_read_default_file=$default_file");
+my $cursor = $conn->prepare("
+SELECT
+  page_title,
+  page_len
+FROM page
+JOIN categorylinks
+ON cl_from = page_id
+WHERE page_namespace = 0
+AND page_is_redirect = 0
+AND cl_to = '살아있는_사람'
+ORDER BY page_len ASC
+LIMIT 1000;
+");
+$cursor->execute();
+
+my $i = 1;
+my @output = ();
+while (my $row = $cursor->fetchrow_hashref()) {
+	my $page_title = sprintf("[[%s]] || %s", $row->{'page_title'}, $row->{'page_len'});
+	my $table_row = sprintf("| %d\n| %s\n|-", $i, $page_title);
+	push @output, $table_row;
+	$i++;
+}
+
+setlocale(LC_TIME, $^O eq 'MSWin32' ? "Korean_Korea.utf8" : "ko_KR.utf8");
+$ENV{TZ} = $timezone_area;
+my $current_of = localtime->strftime($timezone_str);;
+my $final_result = sprintf($report_template, $current_of, join("\n", @output));
+
+$cursor->finish();
+$conn->disconnect();
+
+printf("%s", $final_result);
+
