7297abd3f95bfc9af8957f6de99ffe7ea0d92501
ykhwong <ykhwong@daum.net>
2018-09-15 17:02:59 +0900

    [ADD] 2nd version of uncategorized pages

---
+++ misc/uncategorized_category2.sql
@@ -0,0 +1,31 @@
+SELECT
+  page_title,
+  page_len,
+  cat_pages,
+  rev_timestamp,
+  rev_user_text
+FROM revision
+JOIN
+(SELECT
+   page_id,
+   page_title,
+   page_len,
+   cat_pages
+ FROM category
+ RIGHT JOIN page
+ ON cat_title = page_title
+ LEFT JOIN categorylinks
+ ON page_id = cl_from
+ WHERE cl_from IS NULL
+ AND page_namespace = 14
+ AND page_is_redirect = 0) AS pagetmp
+ON rev_page = pagetmp.page_id
+AND rev_timestamp = (SELECT
+                       MAX(rev_timestamp)
+                     FROM revision AS last 
+                     WHERE last.rev_page = pagetmp.page_id)
+WHERE NOT EXISTS (SELECT 1 FROM templatelinks WHERE tl_from = page_id AND tl_namespace = 10
+AND ( tl_title = '분류_필요' )
+)
+;
+
