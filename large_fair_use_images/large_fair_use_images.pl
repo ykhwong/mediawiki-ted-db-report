1aec2dbbe7505cd083fac66cc15f15804940b9a7
ykhwong <ykhwong@daum.net>
2019-06-06 13:51:20 +0900

    [FIX] rev_text_user deprecated

--- large_fair_use_images/large_fair_use_images.pl
+++ large_fair_use_images/large_fair_use_images.pl
@@ -15,9 +15,7 @@ my $timezone_str = '%Y년 %-m월 %-d일 (%a) %H:%M (KST)';
 my $timezone_area = 'Asia/Seoul';
 my $report_template = q(
 마지막 갱신: <onlyinclude>%s</onlyinclude>
-
 아래는 [[위키백과:비자유_저작물의_공정한_이용#이미지]]에서 설명하는 비자유 그림 중에 지나치게 큰 크기의 그림 파일을 나열합니다. (저장 기준임. 표시 기준의 2배)
-
 );
 
 my $report_template1 = q(
@@ -27,22 +25,31 @@ my $report_template1 = q(
 ! 문서 이름
 ! 가로
 ! 세로
-! 생성자
+<!-- ! 생성자 -->
 |-
 %s
 |}
-
 );
 
 my $conn = DBI->connect("DBI:mysql:database=$dbname;host=$host;mysql_read_default_file=$default_file");
+#my $cursor = $conn->prepare(q(
+#SELECT * FROM
+#( SELECT page.page_title, image.img_width, image.img_height, rev_user_text FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
+#categorylinks.cl_to = '비자유_로고' ) AS pt
+#WHERE EXISTS (
+#  SELECT 1 FROM image WHERE pt.page_title=img_name AND IMG_MAJOR_MIME='image' AND (IMG_WIDTH*IMG_HEIGHT)>20000
+#);
+#));
+
 my $cursor = $conn->prepare(q(
 SELECT * FROM
-( SELECT page.page_title, image.img_width, image.img_height, rev_user_text FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
+( SELECT page.page_title, image.img_width, image.img_height FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
 categorylinks.cl_to = '비자유_로고' ) AS pt
 WHERE EXISTS (
   SELECT 1 FROM image WHERE pt.page_title=img_name AND IMG_MAJOR_MIME='image' AND (IMG_WIDTH*IMG_HEIGHT)>20000
 );
 ));
+
 $cursor->execute();
 
 my $i = 1;
@@ -51,21 +58,31 @@ while (my $row = $cursor->fetchrow_hashref()) {
 	my $page_title = sprintf("[[:파일:%s]]", $row->{'page_title'});
 	my $img_width = sprintf("%s", $row->{'img_width'});
 	my $img_height = sprintf("%s", $row->{'img_height'});
-	my $author = sprintf("%s", $row->{'rev_user_text'});
-	my $table_row = sprintf("| %d || %s || %d || %d || %s\n|-", $i, $page_title, $img_width, $img_height, $author);
+	#my $author = sprintf("%s", $row->{'rev_user_text'});
+	#my $table_row = sprintf("| %d || %s || %d || %d || %s\n|-", $i, $page_title, $img_width, $img_height, $author);
+	my $table_row = sprintf("| %d || %s || %d || %d\n|-", $i, $page_title, $img_width, $img_height);
 	if ($row->{'page_title'} =~ /\.svg$/i) { next; }
 	push @output, $table_row;
 	$i++;
 }
 
+#$cursor = $conn->prepare(q(
+#SELECT * FROM
+#( SELECT page.page_title, image.img_width, image.img_height, rev_user_text FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
+#categorylinks.cl_to = '비자유_건축물' ) AS pt
+#WHERE EXISTS (
+#  SELECT 1 FROM image WHERE pt.page_title=img_name AND IMG_MAJOR_MIME='image' AND ((IMG_WIDTH*IMG_HEIGHT)>200000 OR IMG_WIDTH>400 OR IMG_HEIGHT>1200)
+#);
+#));
 $cursor = $conn->prepare(q(
 SELECT * FROM
-( SELECT page.page_title, image.img_width, image.img_height, rev_user_text FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
+( SELECT page.page_title, image.img_width, image.img_height FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
 categorylinks.cl_to = '비자유_건축물' ) AS pt
 WHERE EXISTS (
   SELECT 1 FROM image WHERE pt.page_title=img_name AND IMG_MAJOR_MIME='image' AND ((IMG_WIDTH*IMG_HEIGHT)>200000 OR IMG_WIDTH>400 OR IMG_HEIGHT>1200)
 );
 ));
+
 $cursor->execute();
 
 $i = 1;
@@ -73,15 +90,28 @@ while (my $row = $cursor->fetchrow_hashref()) {
 	my $page_title = sprintf("[[:파일:%s]]", $row->{'page_title'});
 	my $img_width = sprintf("%s", $row->{'img_width'});
 	my $img_height = sprintf("%s", $row->{'img_height'});
-	my $author = sprintf("%s", $row->{'rev_user_text'});
-	my $table_row = sprintf("| %d || %s || %d || %d || %s\n|-", $i, $page_title, $img_width, $img_height, $author);
+	#my $author = sprintf("%s", $row->{'rev_user_text'});
+	#my $table_row = sprintf("| %d || %s || %d || %d || %s\n|-", $i, $page_title, $img_width, $img_height, $author);
+	my $table_row = sprintf("| %d || %s || %d || %d\n|-", $i, $page_title, $img_width, $img_height);
 	if ($row->{'page_title'} =~ /\.svg$/i) { next; }
 	push @output2, $table_row;
 	$i++;
 }
+#$cursor = $conn->prepare(q(
+#SELECT * FROM
+#( SELECT page.page_title, image.img_width, image.img_height, rev_user_text FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
+#categorylinks.cl_to = '음반_표지' ) AS pt
+#WHERE EXISTS (
+#  SELECT 1 FROM image WHERE pt.page_title=img_name AND IMG_MAJOR_MIME='image' AND (
+#   ( IMG_WIDTH > IMG_HEIGHT AND IMG_HEIGHT > 500 ) OR
+#   ( IMG_WIDTH < IMG_HEIGHT AND IMG_WIDTH > 500 ) OR
+#   ( IMG_WIDTH = IMG_HEIGHT AND IMG_WIDTH > 500 )
+#  )
+#);
+#));
 $cursor = $conn->prepare(q(
 SELECT * FROM
-( SELECT page.page_title, image.img_width, image.img_height, rev_user_text FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
+( SELECT page.page_title, image.img_width, image.img_height FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
 categorylinks.cl_to = '음반_표지' ) AS pt
 WHERE EXISTS (
   SELECT 1 FROM image WHERE pt.page_title=img_name AND IMG_MAJOR_MIME='image' AND (
@@ -91,6 +121,7 @@ WHERE EXISTS (
   )
 );
 ));
+
 $cursor->execute();
 
 $i = 1;
@@ -98,22 +129,34 @@ while (my $row = $cursor->fetchrow_hashref()) {
 	my $page_title = sprintf("[[:파일:%s]]", $row->{'page_title'});
 	my $img_width = sprintf("%s", $row->{'img_width'});
 	my $img_height = sprintf("%s", $row->{'img_height'});
-	my $author = sprintf("%s", $row->{'rev_user_text'});
+	#my $author = sprintf("%s", $row->{'rev_user_text'});
 
 	if ($img_width eq $img_height) {
 		$img_width = "'''" . $img_width . "'''";
 		$img_height = "'''" . $img_height . "'''";
 	}
 
-	my $table_row = sprintf("| %d || %s || %s || %s || %s\n|-", $i, $page_title, $img_width, $img_height, $author);
+	#my $table_row = sprintf("| %d || %s || %s || %s || %s\n|-", $i, $page_title, $img_width, $img_height, $author);
+	my $table_row = sprintf("| %d || %s || %d || %d\n|-", $i, $page_title, $img_width, $img_height);
 	if ($row->{'page_title'} =~ /\.svg$/i) { next; }
 	push @output3, $table_row;
 	$i++;
 }
 
+#$cursor = $conn->prepare(q(
+#SELECT * FROM
+#( SELECT page.page_title, image.img_width, image.img_height, rev_user_text FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
+#( categorylinks.cl_to = '영화_장면' OR categorylinks.cl_to = '뮤직_비디오_장면' OR categorylinks.cl_to = '웹_페이지_스크린샷' OR categorylinks.cl_to = '텔레비전_장면' OR
+#  categorylinks.cl_to = '마이크로소프트_제품의_스크린샷' OR categorylinks.cl_to = '맥_소프트웨어의_스크린샷' OR categorylinks.cl_to = '비자유_비디오_게임_스크린샷' OR
+#  categorylinks.cl_to = '비자유_소프트웨어_스크린샷' OR categorylinks.cl_to = '윈도우_소프트웨어의_스크린샷'
+# ) ) AS pt
+#WHERE EXISTS (
+#  SELECT 1 FROM image WHERE pt.page_title=img_name AND IMG_MAJOR_MIME='image' AND (IMG_WIDTH*IMG_HEIGHT)>80000
+#);
+#));
 $cursor = $conn->prepare(q(
 SELECT * FROM
-( SELECT page.page_title, image.img_width, image.img_height, rev_user_text FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
+( SELECT page.page_title, image.img_width, image.img_height FROM page JOIN categorylinks ON page.page_id = categorylinks.cl_from JOIN image ON page.page_title=image.img_name JOIN revision_userindex ON page_id = rev_page WHERE rev_timestamp > 1 AND rev_parent_id = '0' AND page.page_namespace = 6 AND page.page_is_redirect = 0  AND
 ( categorylinks.cl_to = '영화_장면' OR categorylinks.cl_to = '뮤직_비디오_장면' OR categorylinks.cl_to = '웹_페이지_스크린샷' OR categorylinks.cl_to = '텔레비전_장면' OR
   categorylinks.cl_to = '마이크로소프트_제품의_스크린샷' OR categorylinks.cl_to = '맥_소프트웨어의_스크린샷' OR categorylinks.cl_to = '비자유_비디오_게임_스크린샷' OR
   categorylinks.cl_to = '비자유_소프트웨어_스크린샷' OR categorylinks.cl_to = '윈도우_소프트웨어의_스크린샷'
@@ -122,6 +165,7 @@ WHERE EXISTS (
   SELECT 1 FROM image WHERE pt.page_title=img_name AND IMG_MAJOR_MIME='image' AND (IMG_WIDTH*IMG_HEIGHT)>80000
 );
 ));
+
 $cursor->execute();
 
 $i = 1;
@@ -129,8 +173,9 @@ while (my $row = $cursor->fetchrow_hashref()) {
 	my $page_title = sprintf("[[:파일:%s]]", $row->{'page_title'});
 	my $img_width = sprintf("%s", $row->{'img_width'});
 	my $img_height = sprintf("%s", $row->{'img_height'});
-	my $author = sprintf("%s", $row->{'rev_user_text'});
-	my $table_row = sprintf("| %d || %s || %d || %d || %s\n|-", $i, $page_title, $img_width, $img_height, $author);
+	#my $author = sprintf("%s", $row->{'rev_user_text'});
+	#my $table_row = sprintf("| %d || %s || %d || %d || %s\n|-", $i, $page_title, $img_width, $img_height, $author);
+	my $table_row = sprintf("| %d || %s || %d || %d \n|-", $i, $page_title, $img_width, $img_height);
 	if ($row->{'page_title'} =~ /\.svg$/i) { next; }
 	push @output4, $table_row;
 	$i++;
