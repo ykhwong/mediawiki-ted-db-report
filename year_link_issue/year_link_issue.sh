9379833c7933dc9df118fefeabc9ad935930c868
ted <ykhwong@daum.net>
2020-04-16 23:26:00 +0900

    [FIX] Improved the existing table

--- year_link_issue/year_link_issue.sh
+++ year_link_issue/year_link_issue.sh
@@ -1,7 +1,7 @@
 LANG=ko_KR.utf8
 UTIL='expr wget date sed perl grep cat'
-URL1='https://ko.wikipedia.org/w/index.php?title=%ED%8A%B9%EC%88%98:%EA%B2%80%EC%83%89&limit=5000&offset=0&ns0=1&search=insource%3A%2F%5C%5B%5C%5B%5B0-9%5D%7B1%2C4%7D%EB%85%84%5C%7C'
-URL2='%EB%85%84%5C%5D%5C%5D%2F&advancedSearch-current={}'
+URL1='https://ko.wikipedia.org/w/index.php?title=%ED%8A%B9%EC%88%98:%EA%B2%80%EC%83%89&limit=5000&offset=0&profile=default&search=insource%3A%2F%5C%5B%5C%5B%5B0-9%5D%7B1%2C4%7D%EB%85%84%5C%7C'
+URL2='%EB%85%84%5C%5D%5C%5D%2F&advancedSearch-current={}&ns0=1'
 CNT=1
 timezone_area='Asia/Seoul'
 timezone_str='%Y년 %-m월 %-d일 (%a) %H:%M (KST)'
@@ -15,8 +15,7 @@ do
 		exit $RETCODE
 	fi
 done
-FULLCNT=`date +'%Y'`
-FULLCNT=`expr ${FULLCNT} + 1`
+FULLCNT="2... 1... 9.. 8.. 7.. 6.. 5.. 4.. 3.. 2.. 1.. 9. 8. 7. 6. 5. 4. 3. 2. 1. 9 8 7 6 5 4 3 2 1"
 
 export TZ=":${timezone_area}"
 DATE=`date +"${timezone_str}"`
@@ -29,26 +28,53 @@ cat <<"END"
 ** <nowiki>[[2020년|2020년]]</nowiki> → <nowiki>[[2020년]]</nowiki>으로 수정 필요
 ** <nowiki>[[2018년|2020년]]</nowiki> → <nowiki>[[2018년]] 또는 [[2020년]]</nowiki>으로 수정 필요
 END
-while true
+IFS=' '
+for customitem in ${FULLCNT}
 do
-FULLCNT=`expr $FULLCNT - 1`
-if [ $FULLCNT -eq 0 ]; then
-	break
-fi
 echo ''
-echo "== ${FULLCNT}년 =="
+echo "== `echo ${customitem}년 | sed 's/\./X/g'` =="
 echo '{| class="wikitable sortable plainlinks" style="width:100%; margin:auto;"'
 echo '|- style="white-space:nowrap;"'
-echo '! 순번 !! 문서 이름'
+echo '! 순번 !! 문서 이름 !! 일치'
 echo '|-'
 IFS='
 '
 CNT=1
-for sth in `wget -qO- ${URL1}${FULLCNT}${URL2} | perl -pe 's/title="/\ntitle="/mg' | grep -P '^title=' | perl -pe 's/(^title="|" +data-serp-pos=.*)//g' | grep -vP '>' | perl -pe 's/^\*+$//'`
+HOLD=0
+for sth in `wget -qO- ${URL1}${customitem}${URL2}`
 do
-	echo "| $CNT || [[${sth}]]"
-	echo "|-"
-	CNT=`expr $CNT + 1`
+	if [ $HOLD -eq 1 ]; then
+		if [ `echo $sth | grep -cP '<span class="searchmatch">'` -ne 0 ]; then
+			SEARCHMATCH=`echo $sth | perl -pe 's/<\/span>.*//' | perl -pe 's/.*\Q<span class="searchmatch">\E//mg' | perl -pe 's/<\/span>.*//'`
+			echo "<nowiki>$SEARCHMATCH</nowiki>"
+			echo "|-"
+			HOLD=0
+		fi
+	fi
+	for sth2 in `echo $sth | grep " data-serp-pos=" | perl -pe 's/title="/\ntitle="/mg' | grep -P '^title='`
+	do
+		SEARCHMATCH="-"
+		if [ $HOLD -eq 1 ]; then
+			echo "-"
+			echo "|-"
+			HOLD=0
+		fi
+		TITLE=`echo $sth2 | perl -pe 's/(^title="|" +data-serp-pos=.*)//g' | grep -vP '>' | perl -pe 's/^\*+$//'`
+		if [ `echo $TITLE | grep -Pc '\S'` -ne 0 ]; then
+			for sth3 in `echo $sth2 | grep '<span class="searchmatch">'`
+			do
+				SEARCHMATCH=`echo $sth2 | perl -pe 's/.*\Q<span class="searchmatch">\E//mg' | perl -pe 's/<\/span>.*//'`
+			done
+			if [ `echo ${SEARCHMATCH} | grep -cP '^-$'` -ne 0 ]; then
+				echo -n "| $CNT || [[${TITLE}]] || "
+				HOLD=1
+			else
+				echo "| $CNT || [[${TITLE}]] || <nowiki>${SEARCHMATCH}</nowiki>"
+				echo "|-"
+			fi
+			CNT=`expr $CNT + 1`
+		fi
+	done
 done
 echo "|}"
 if [ $CNT -ne 1 ]; then
