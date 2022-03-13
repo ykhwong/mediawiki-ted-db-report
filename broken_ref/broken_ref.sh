bb4e7ac99c119f0f4cb4722fabf977537057c067
ted <ykhwong@daum.net>
2022-03-13 15:42:35 +0900

    [FIX] URL

--- broken_ref/broken_ref.sh
+++ broken_ref/broken_ref.sh
@@ -1,6 +1,6 @@
 LANG=ko_KR.utf8
 UTIL='expr wget date sed perl grep'
-URL='https://ko.wikipedia.org/w/index.php?title=%ED%8A%B9%EC%88%98:%EA%B2%80%EC%83%89&limit=5000&offset=0&profile=default&search=%22%EC%9D%B4%EB%A6%84%EC%9D%84+%EA%B0%80%EC%A7%84+%EC%A3%BC%EC%84%9D%EC%97%90+%EC%A0%9C%EA%B3%B5%ED%95%9C+%ED%85%8D%EC%8A%A4%ED%8A%B8%EA%B0%80+%EC%97%86%EC%8A%B5%EB%8B%88%EB%8B%A4%22&advancedSearch-current=%7B%7D&ns0=1&searchToken=9n2dczqm4013jdxlrcaw5b3m2'
+URL='https://ko.wikipedia.org/w/index.php?title=%ED%8A%B9%EC%88%98:%EA%B2%80%EC%83%89&limit=5000&offset=0&ns0=1&search=%22%EC%9D%B4%EB%A6%84%EC%9D%84+%EA%B0%80%EC%A7%84+%EC%A3%BC%EC%84%9D%EC%97%90+%ED%85%8D%EC%8A%A4%ED%8A%B8%EA%B0%80+%EC%97%86%EC%8A%B5%EB%8B%88%EB%8B%A4%22'
 CNT=1
 timezone_area='Asia/Seoul'
 timezone_str='%Y년 %-m월 %-d일 (%a) %H:%M (KST)'
