742a5ae0433cc7fb12a1728ddfb460710e65b38b
ted <ykhwong@daum.net>
2022-05-13 15:15:46 +0900

    [FIX] Update MariaDB SQL

--- misc/get_articles_per_user.sh
+++ misc/get_articles_per_user.sh
@@ -4,7 +4,7 @@ HOST=kowiki.analytics.db.svc.eqiad.wmflabs
 DBNAME=kowiki_p
 DEFAULT_FILE='~/replica.my.cnf'
 MYSQL_CMD="mysql --defaults-file=$DEFAULT_FILE -h $HOST $DBNAME"
-USERID=Ykhwong
+USERID=A.TedBot
 
 for cmd in $UTIL
 do
@@ -16,20 +16,12 @@ do
         fi
 done
 
-REV_USER=$(echo 'SELECT REV_USER FROM `revision` WHERE REV_USER_TEXT = '"'${USERID}'"' LIMIT 1;' | $MYSQL_CMD | tail -1)
+ACTOR_ID=$(echo 'SELECT actor_id from actor where actor_name='"'${USERID}'"' LIMIT 1;' | $MYSQL_CMD | tail -1)
 RETCODE=$?
 if [ $RETCODE -ne 0 ]; then
 	exit $RETCODE
 fi
 
-echo "SELECT DISTINCT page_namespace AS namespace, 'rev' AS type, page_title AS page_title,
-                page_len, page_is_redirect, rev_timestamp AS rev_timestamp,
-                rev_user, rev_user_text AS username, rev_len, rev_id 
-            FROM "'`kowiki_p`.`page`
-            JOIN `kowiki_p`.`revision_userindex` ON page_id = rev_page'"
-            WHERE  rev_user = '${REV_USER}' AND rev_timestamp > 1
-            AND rev_parent_id = '0'  AND page_namespace = '0'
-            AND page_is_redirect = '0' 
-            ORDER BY rev_id DESC
-;" | $MYSQL_CMD
-
+echo "SELECT DISTINCT page_namespace AS \`namespace\`, 'rev' AS \`type\`, page_title, page_is_redirect, page_len
+FROM page JOIN revision ON page_id = rev_page WHERE rev_actor = ${ACTOR_ID} AND rev_parent_id = '0' AND page_namespace = '0' AND page_is_redirect = '0'
+ORDER BY rev_timestamp DESC;" | $MYSQL_CMD
