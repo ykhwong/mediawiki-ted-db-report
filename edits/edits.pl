de90353bde631a5b3c2ae878e41d862862b71185
ted <ykhwong@daum.net>
2022-07-19 00:03:58 +0900

    [ADD] Edits

---
+++ edits/edits.pl
@@ -0,0 +1,161 @@
+use strict;
+no warnings;
+use MediaWiki::Bot;
+use Time::Piece;
+use POSIX qw(tzset);
+use POSIX qw(locale_h);
+use Encode qw(decode encode);
+#use locale;
+use DBI;
+use User::pwent;
+
+my $host='kowiki.analytics.db.svc.eqiad.wmflabs';
+my $dbname='kowiki_p';
+my $default_file='/data/project/tedbot/replica.my.cnf';
+my $timezone_str = '%Y년 %-m월 %-d일 (%a) %H:%M (KST)';
+my $timezone_area = 'Asia/Seoul';
+
+my $conn = DBI->connect("DBI:mysql:database=$dbname;host=$host;mysql_read_default_file=$default_file");
+my $cursor = $conn->prepare("
+SELECT a.user_name, a.user_editcount, b.ug_group
+	FROM user AS a
+	LEFT JOIN user_groups AS b
+	ON ug_user = user_id
+	WHERE user_editcount > '4999'
+	ORDER BY user_editcount desc;
+");
+$cursor->execute();
+
+my $bot_ko = MediaWiki::Bot->new({
+	assert      => 'user',
+	protocol    => 'https',
+	host        => 'ko.wikipedia.org',
+	agent       => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.66 Safari/537.36 Edg/80.0.361.40'
+}) or die(0);
+my $timezone_str = '%Y년 %-m월 %-d일 (%a) %H:%M (KST)';
+my @placeholder;
+my @excluded;
+my $hold = 0;
+foreach my $ls (split /\n/, $bot_ko->get_text(decode('utf-8', "위키백과:기여가 많은 위키백과 사용자 명단/자리 채움"))) {
+	if ($ls =~ /^\s*<pre/i) {
+		$hold = 1;
+		next;
+	}
+	if ($hold eq 1 && $ls !~ /^\s*\<\/pre\>/i) {
+		$ls =~ s/ +/_/g;
+		push @placeholder, $ls;
+	}
+}
+
+$hold=0;
+foreach my $ls (split /\n/, $bot_ko->get_text(decode('utf-8', "위키백과:기여가 많은 위키백과 사용자 명단/제외 목록"))) {
+	if ($ls =~ /^\s*<pre/i) {
+		$hold = 1;
+		next;
+	}
+	if ($hold eq 1 && $ls !~ /^\s*\<\/pre\>/i) {
+		$ls =~ s/ +/_/g;
+		push @excluded, $ls;
+	}
+
+}
+
+setlocale(LC_TIME, $^O eq 'MSWin32' ? "Korean_Korea.utf8" : "ko_KR.utf8");
+$ENV{TZ} = $timezone_area;
+my $current_of = localtime->strftime($timezone_str);
+print "* 갱신 일자: " . $current_of . "\n";
+
+my $working = 0;
+my $rows = $cursor->rows;
+my $tmp_list = "";
+while (my $row = $cursor->fetchrow_hashref()) {
+	$tmp_list .= $row->{'user_name'} . "\t" . $row->{"user_editcount"} . "\t" . $row->{"ug_group"} . "\n";
+}
+
+my $prev_name = "";
+my $first=1;
+my $wikitext;
+foreach my $ls (split /\n/, $tmp_list) {
+	my $cur_name = (split /\t/, $ls)[0];
+	my $cur_name2 = $cur_name; $cur_name2 =~ s/ +/_/g;
+	my $contrib = (split /\t/, $ls)[1];
+	my $right = (split /\t/, $ls)[2];
+	my $is_placeholder = 0;
+	if ($right =~ /bot/) {
+		next;
+	}
+	if ($cur_name2 =~ /(bot|봇)$/i) {
+		next;
+	}
+
+	my $skip = 0;
+	foreach my $itm (@excluded) {
+		if ($itm eq ${cur_name2}) {
+			$skip = 1;
+			last;
+		}
+	}
+	if ($skip eq 1) {
+		next;
+	}
+	foreach my $itm (@placeholder) {
+		if ($itm eq ${cur_name2}) {
+			$is_placeholder = 1;
+			last;
+		}
+	}
+	$right =~ s/interface-admin/인터페이스 관리자/i;
+	$right =~ s/bot/봇/i;
+	$right =~ s/sysop/관리자/i;
+	$right =~ s/checkuser/검사관/i;
+	$right =~ s/oversight/기록보호자/i;
+	$right =~ s/suppress/기록보호자/i;
+	$right =~ s/extendedconfirmed/자동 인증/i;
+	$right =~ s/autopatrolled/점검 면제/i;
+	$right =~ s/uploader/업로더/i;
+	$right =~ s/rollbacker/일괄 되돌리기 기능 사용자/i;
+	$right =~ s/bureaucrat/사무관/i;
+	if ($first eq 1) {
+		$wikitext = '# (' . $contrib . '회) ';
+		if ($is_placeholder eq 1) {
+			$wikitext .= '[자리 채움]';
+		} else {
+			$wikitext .= '{{사용자2|1=' . $cur_name2 . '}}';
+		}
+		if ($is_placeholder eq 0 && $right =~ /\S/) {
+			$wikitext .= ' (' . $right;
+		}
+		$first = 0;
+		$prev_name = $cur_name;
+		next;
+	}
+	if ($is_placeholder eq 0) {
+		if ($prev_name eq $cur_name) {
+			$wikitext .= ', ' . $right;
+		} else {
+			$wikitext .= "\n" . '# (' . $contrib . '회) ' . '{{사용자2|1=' . $cur_name2 . '}}';
+			if ($right =~ /\S/) {
+				$wikitext .= ' (' . $right;
+			}
+		}
+	}
+	$prev_name = $cur_name;
+}
+
+$cursor->finish();
+$conn->disconnect();
+
+my $newWikitext;
+foreach my $ls (split /\n/, $wikitext) {
+	if ($ls !~ /(\]|\}\})\s*$/) {
+		$ls .= ")";
+	}
+	$ls =~ s/, 자동 인증, /, /;
+	$ls =~ s/\Q (자동 인증)\E//;
+	$ls =~ s/\Q (자동 인증, \E/ (/;
+	$ls =~ s/\Q, 자동 인증)\E/)/;
+
+	$newWikitext .= $ls . "\n";
+}
+
+print $newWikitext . "\n";
