5d8f211f043e98a045d0b7bc98023c2621eb5c02
ted <ykhwong@daum.net>
2023-03-01 14:47:49 +0900

    [FIX] Check seed syntax: External links

--- namuwiki_syntax/namuwiki_syntax.sh
+++ namuwiki_syntax/namuwiki_syntax.sh
@@ -2,6 +2,7 @@ LANG=ko_KR.utf8
 UTIL='expr wget date sed perl grep cat'
 URL='https://ko.wikipedia.org/w/index.php?title=특수:검색&limit=5000&offset=0&profile=default&search=insource%3A%2F%28%5C%5B%5C%2A+%7C%5B%5E%5C%5B%5D%5C%5B%28목차%7C각주%7Cyoutube%29%5C%5D%7C%5C%7B%23색코드%7C%5C%5Binclude%5C%28%7C%5C%5Bbr%5C%5D%7C%5C%7B%5C%7B%5C%7B%5C%2B%5B0-9%5D%29%2F&advancedSearch-current=%7B"namespaces"%3A%5B0%5D%7D'
 URL2='https://ko.wikipedia.org/w/index.php?search=insource%3A%2Fhttps*%3A%5C%2F%5C%2Fnamu%5C.wiki%2F+-hastemplate%3A%22%ED%8A%B9%EC%A0%95%ED%8C%90+%EC%82%AD%EC%A0%9C+%EC%8B%A0%EC%B2%AD%22+-hastemplate%3A%22%EC%82%AD%EC%A0%9C+%EC%8B%A0%EC%B2%AD%22+-intitle%3A%2F%EC%9C%84%ED%82%A4%2F&title=%ED%8A%B9%EC%88%98%3A%EA%B2%80%EC%83%89&go=%EB%B3%B4%EA%B8%B0&ns0=1'
+URL3='https://ko.wikipedia.org/w/index.php?title=%ED%8A%B9%EC%88%98:%EA%B2%80%EC%83%89&limit=5000&offset=0&ns0=1&search=insource%3A%2F%5C%5B%5C%5Bhttps%2A%3A%5C%2F%5C%2F%2F'
 CNT=1
 timezone_area='Asia/Seoul'
 timezone_str='%Y년 %-m월 %-d일 (%a) %H:%M (KST)'
@@ -76,5 +77,30 @@ do
 	CNT=`expr $CNT + 1`
 done
 echo "|}"
+
+cat <<"END"
+
+== 외부 링크가 더 시드 문법으로 걸린 문서 ==
+{| class="wikitable" style="text-align:center; width: 750px;"
+! width="300px" | 더 시드 문법
+! width="450px" |[[미디어위키]] 문법
+|-
+|<code><nowiki>[[http://example.org|예제]]</nowiki></code>||<code><nowiki>[http://example.org 예제]</nowiki></code>
+|-
+|<code><nowiki>[[https://example.org|예제]]</nowiki></code>||<code><nowiki>[https://example.org 예제]</nowiki></code>
+|}
+END
+IFS='
+'
+for sth in `wget -qO- $URL3 | perl -pe 's/title="/\ntitle="/mg' | grep -P '^title=' | perl -pe 's/(^title="|" +data-serp-pos=.*)//g' | grep -vP '>|\[' | perl -pe 's/^\*+$|(More options|더 많은 옵션)"//'`
+do
+	echo "| $CNT || [[${sth}]]"
+	echo "|-"
+	CNT=`expr $CNT + 1`
+done
+echo "|}"
+
+
+
 
 unset IFS
