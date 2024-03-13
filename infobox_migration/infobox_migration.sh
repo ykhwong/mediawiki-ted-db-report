45f8a184700e7a776929af3e400e4af5cd075863
ted <ykhwong@daum.net>
2024-03-13 09:33:43 +0900

    Add URL2

--- infobox_migration/infobox_migration.sh
+++ infobox_migration/infobox_migration.sh
@@ -1,6 +1,7 @@
 LANG=ko_KR.utf8
 UTIL='expr wget date sed perl grep'
-URL='https://ko.wikipedia.org/w/index.php?title=%ED%8A%B9%EC%88%98:%EA%B2%80%EC%83%89&limit=500&offset=0&ns10=1&search=hastemplate%3A%22%EC%A0%95%EB%B3%B4%EC%83%81%EC%9E%90+%EC%B9%B8%22+-intitle%3A%2F%5C%2F%28%EC%84%A4%EB%AA%85%EB%AC%B8%EC%84%9C%7C%EC%97%B0%EC%8A%B5%EC%9E%A5%29%2F+insource%3A%2F%5C%7B%5C%7B%EC%A0%95%EB%B3%B4%EC%83%81%EC%9E%90%5B+_%5D%2B%EC%B9%B8%2F&searchToken=e72kigfwsfjaquwyrleue0jb3'
+URL1='https://ko.wikipedia.org/w/index.php?title=%ED%8A%B9%EC%88%98:%EA%B2%80%EC%83%89&limit=500&offset=0&ns10=1&search=hastemplate%3A%22%EC%A0%95%EB%B3%B4%EC%83%81%EC%9E%90+%EC%B9%B8%22+-intitle%3A%2F%5C%2F%28%EC%84%A4%EB%AA%85%EB%AC%B8%EC%84%9C%7C%EC%97%B0%EC%8A%B5%EC%9E%A5%29%2F+insource%3A%2F%5C%7B%5C%7B%EC%A0%95%EB%B3%B4%EC%83%81%EC%9E%90%5B+_%5D%2B%EC%B9%B8%2F&searchToken=e72kigfwsfjaquwyrleue0jb3'
+URL2='https://ko.wikipedia.org/w/index.php?title=%ED%8A%B9%EC%88%98:%EA%B2%80%EC%83%89&limit=5000&offset=0&ns10=1&search=insource%3A%2F%5C%7B%5C%7C+%2Aclass%3D%5C%22infobox%2F+-hastemplate%3A%22%EC%A0%95%EB%B3%B4%EC%83%81%EC%9E%90+%EC%B9%B8%22'
 
 out() {
 	wget -qO- $1 | perl -pe 's/data-serp-pos="\d+">/\ntitle=/mg' | grep -P '^title=' | sed 's/^title=//' | perl -pe 's/<\/a>.+//mg'
@@ -24,14 +25,31 @@ export TZ=":${timezone_area}"
 DATE=`date +"${timezone_str}"`
 echo '마지막 갱신: <onlyinclude>'$DATE'</onlyinclude>'
 echo ''
+echo '== 정보상자 칸 틀 사용 =='
 echo '{| class="wikitable sortable plainlinks" style="width:100%; margin:auto;"'
 echo '|- style="white-space:nowrap;"'
 echo '! 순번 !! 문서 이름'
 echo '|-'
 IFS='
 '
-for sth in `( out $URL ) | sort -u`
+for sth in `( out $URL1 ) | sort -u`
 
+do
+	echo "| $CNT || [[$sth]]"
+	echo "|-"
+	CNT=`expr $CNT + 1`
+done
+echo "|}"
+
+CNT=1
+echo '== 정보상자 칸 틀 미사용 =='
+echo '{| class="wikitable sortable plainlinks" style="width:100%; margin:auto;"'
+echo '|- style="white-space:nowrap;"'
+echo '! 순번 !! 문서 이름'
+echo '|-'
+IFS='
+'
+for sth in `( out $URL2 ) | sort -u`
 do
 	echo "| $CNT || [[$sth]]"
 	echo "|-"
